5172cd0b5c98f2f8e336ca5ea7addc93f4555f60
Jordan Aranda Tejada <jordan.aranda@me.com>
2013-09-20 12:23:40 +0200

    Se modifica el servidor

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,42 +1,31 @@
 package server;
 
-import java.io.EOFException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.UnknownHostException;
-
-import javax.swing.JOptionPane;
+import java.util.Vector;
 
 import utils.Properties;
-import database.DataBase;
-import entities.Request;
 
 /**
  * @author Jordan Aranda Tejada
  */
 public class Server implements Runnable {
 
-	private boolean				running;
-	private ServerSocket		serverSocket;
-	private Socket				connection;
-	private ObjectOutputStream	output;
-	private ObjectInputStream	input;
-	private long				connections;
+	private boolean			running;
+	private ServerSocket	serverSocket;
+	private Vector<Thread>	connectionsThreads;
 
 	/**
 	 * Creates the server
 	 */
-	public Server()
+	public Server(final int PORT, final int backlog, final InetAddress IP)
 	{
-		this.connections = 0;
 		try
 		{
-			serverSocket = new ServerSocket(Properties.getServerPORT(), 3000,
-			InetAddress.getByName(Properties.getServerIP()));
+			serverSocket = new ServerSocket(PORT, backlog, IP);
+			connectionsThreads = new Vector<Thread>();
 		}
 		catch (UnknownHostException e)
 		{
@@ -51,88 +40,18 @@ public class Server implements Runnable {
 
 	private void waitConnections() throws IOException
 	{
-		connection = serverSocket.accept();
+		connectionsThreads.add(new Thread(new ServerThread(serverSocket
+		.accept())));
+		connectionsThreads.get(connectionsThreads.size()).start();
+
 		// System.out.println("New connection received from: "+connection.getInetAddress().toString()+" ("+connection.getInetAddress().getHostName()+")");
-		connections++;
-	}
-
-	// Obtenemos los flujos de la conexión
-	private void getStreams() throws IOException
-	{
-		output = new ObjectOutputStream(connection.getOutputStream());
-		output.flush();
-		input = new ObjectInputStream(connection.getInputStream());
-	}
-
-	private void processConnection()
-	{
-		Request request = (Request) obtainInputObject();
-		String command = (String) obtainInputObject();
-		if (ServerMetods.isUpdateComand(command))
-		{
-			sendData(DataBase.getInstance().update(command));
-		}
-		else if (ServerMetods.isConsultComand(command))
-		{
-			String[][] array = null;
-			// try
-			// {
-			// array =
-			// ServerMetods.resultSetToArray(DataBase.getInstance().consult(command),
-			// DataBase.getInstance().countConsult(command));
-			// sendData(array);
-			// }
-			// catch (SQLException e)
-			// {
-			// e.printStackTrace();
-			// }
-		}
-	}
-
-	private Object obtainInputObject()
-	{
-		Object object = null;
-		try
-		{
-			object = input.readObject();
-		}
-		catch (ClassNotFoundException excepcionClaseNoEncontrada)
-		{
-			JOptionPane
-			.showMessageDialog(null, "Unknown type object receibed.", "Error",
-			JOptionPane.ERROR_MESSAGE);
-		}
-		catch (IOException e)
-		{
-			closeConnection();
-			JOptionPane.showMessageDialog(null,
-			"The connection with the server failed.", "Connection failed.",
-			JOptionPane.ERROR_MESSAGE);
-		}
-		return object;
-	}
-
-	public void sendData(Object object)
-	{
-		try
-		{
-			output.writeObject(object);
-			output.flush();
-		}
-		catch (IOException excepcionES)
-		{
-			JOptionPane.showMessageDialog(null, "Error sending object.",
-			"Error", JOptionPane.ERROR_MESSAGE);
-		}
 	}
 
 	private void closeConnection()
 	{
 		try
 		{
-			output.close();
-			input.close();
-			connection.close();
+			serverSocket.close();
 		}
 		catch (IOException excepcionES)
 		{
@@ -145,18 +64,9 @@ public class Server implements Runnable {
 	{
 		while (running)
 		{
-
 			try
 			{
 				waitConnections();
-				getStreams();
-				processConnection();
-			}
-			catch (EOFException excepcionEOF)
-			{
-				JOptionPane.showMessageDialog(null,
-				"The server finished the connection.", "Connection finished",
-				JOptionPane.INFORMATION_MESSAGE);
 			}
 			catch (IOException e)
 			{
@@ -166,7 +76,6 @@ public class Server implements Runnable {
 			{
 				closeConnection();
 			}
-
 		}
 	}
 
@@ -175,7 +84,16 @@ public class Server implements Runnable {
 	 */
 	public static void main(String ... args)
 	{
-		Server server = new Server();
+		Server server = null;
+		try
+		{
+			server = new Server(Properties.getServerPORT(), 3000,
+			InetAddress.getByName(Properties.getServerIP()));
+		}
+		catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
 		Thread server_thread = new Thread(server);
 		server_thread.run();
 	}
